262911c3c544ae30a9f2f02e0cf7902c04043a61
swakraft <hugo.beets2003@gmail.com>
2026-07-24 16:34:50 +0200

    Remplace le module de logging maison par loguru
    Le module custom (ConsoleLogger/FileLogger/Line) reposait sur de
    l'inspection de pile fragile (index de frame en dur) et une astuce de
    réécriture de ligne ANSI non thread/async-safe. loguru fournit une
    solution éprouvée (caller info fiable, rotation/rétention de fichier,
    gestion d'exception) tout en gardant le même rendu console coloré
    ([INFO]/[WARN]/[ERROR]/[CRIT]/[DEBUG]/[GET]/[POST]/[DELETE] + timestamp
    fr + fichier:ligne).
    
    Le middleware HTTP loggue désormais une seule ligne par requête
    (méthode/URL + code HTTP) une fois le handler exécuté, au lieu de
    réécrire la ligne en place dans le terminal. Même principe appliqué au
    chargement des routes v1 (deux lignes de debug au lieu d'une ligne
    réécrite).

--- src/handlers/v1/utils/subapp.py
+++ src/handlers/v1/utils/subapp.py
@@ -5,10 +5,9 @@ v1 = web.Application()
 log.debug("app v1 created")
 routes = web.RouteTableDef()
 
-line = log.debug("Importing routes")
+log.debug("Importing routes")
 from ..import stats, weights, health
-line.add_text("OK")
-line.edit_print()
+log.debug("Routes imported")
 log.debug("done. Adding routes")
 v1.add_routes(routes)
 log.debug("routes added")
